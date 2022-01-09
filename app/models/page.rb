9d191908e65d0390e84c3879f7dea4f75b45bd29
Justin Tanner <work@jwtanner.com>
2022-01-09 12:22:31 -0600

    Added some attributes to the page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,12 @@
 class Page < ApplicationRecord
   has_rich_text :body
+  auto_strip_attributes :title, squish: true
+  before_save :set_slug
+
+  validates_presence_of :title
+
+  private
+  def set_slug
+    self.slug = self.title.parameterize.downcase
+  end
 end
--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save page without title' do
+    page = Page.new
+    assert_not page.save, 'Saved the page without a title'
+  end
+
+  test 'creates a slug based on the title' do
+    page = Page.create(title: 'My Page')
+    assert_equal 'my-page', page.slug
+  end
+
+  test 'strips spaces out of titles' do
+    page = Page.create(title: 'My     Page ')
+    assert_equal 'My Page', page.title
+  end
+
+  test 'strips spaces out of slugs' do
+    page = Page.create(title: 'My     Page ')
+    assert_equal 'my-page', page.slug
+  end
 end
