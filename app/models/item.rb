68b7e9ea02430a38cddf67f3818b47d41d5dff54
Justin Tanner <work@jwtanner.com>
2022-01-11 15:04:30 -0600

    Added a validation to safe guard against sybmols getting into Item.fields

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   friendly_id :title, use: :history
 
   validate :title_present
+  validate :no_symbols_in_fields
 
   def title
     fields.try(:[], 'item_title')
@@ -14,6 +15,12 @@ class Item < ApplicationRecord
   def title_present
     if fields.try(:[], 'item_title').blank?
       errors.add(:fields_item_title, 'Please set an fields[item_title]')
+    end
+  end
+
+  def no_symbols_in_fields
+    if fields.present? && fields.keys.any? { |key| key.class == Symbol }
+      errors.add(:fields, 'No symbols allowed in fields')
     end
   end
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -6,24 +6,44 @@ class ItemTest < ActiveSupport::TestCase
     assert_not item.save, 'Saved the title without a title'
   end
 
+  test 'should never allow a field with a symbol for a key' do
+    item = Item.new
+    item.fields = {}
+    item.fields[:item_title] = 'Bad key'
+    assert_not item.save, 'Saved fields with a symbol as a key'
+  end
+
+  test 'should be able to create field with symbols as keys, but they get converted to strings' do
+    item =
+      Item.create(
+        fields: {
+          item_title: 'Key converted to string',
+          number: 123
+        }
+      )
+
+    assert item.fields.keys.all? { |key| key.is_a?(String) },
+           'Fields keys are not all strings'
+  end
+
   test 'should save fields[item_title] in both fields and title' do
-    item = Item.create(fields: { 'item_title': 'Apple' })
+    item = Item.create(fields: { item_title: 'Apple' })
     assert_equal 'Apple', item.title
   end
 
   test 'titles generates a slug' do
-    item = Item.create(fields: { 'item_title': 'My card' })
+    item = Item.create(fields: { item_title: 'My card' })
     assert_equal 'my-card', item.slug
   end
 
   test 'title trim a squish whitespace' do
-    item = Item.create(fields: { 'item_title': " My \t \t card \n" })
+    item = Item.create(fields: { item_title: " My \t \t card \n" })
     assert_equal 'My card', item.title
     assert_equal 'My card', item.fields['item_title']
   end
 
   test 'item_title trim a squish whitespace' do
-    item = Item.create(fields: { 'item_title': " My \t \t card \n" })
+    item = Item.create(fields: { item_title: " My \t \t card \n" })
     assert_equal 'My card', item.title
     assert_equal 'My card', item.fields['item_title']
   end
