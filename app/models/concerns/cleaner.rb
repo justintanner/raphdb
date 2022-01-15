395d1ef03323500adf8a1c25d2924398024e4cef
Justin Tanner <work@jwtanner.com>
2022-01-15 09:37:27 -0600

    Refactored Cleaner concern again

--- app/models/concerns/cleaner.rb
+++ app/models/concerns/cleaner.rb
@@ -10,27 +10,31 @@ module Cleaner
       column_type = self.type_for_attribute(attribute).type
 
       before_validation do |record|
-        Clean.attribute(record, attribute.to_s, column_type)
+        Clean.attribute(
+          record: record,
+          attribute: attribute.to_s,
+          column_type: column_type
+        )
       end
     end
   end
 end
 
 module Clean
-  def self.attribute(record, attribute, column_type)
+  def self.attribute(record:, attribute:, column_type:)
     if column_type == :jsonb && record[attribute].present?
       record[attribute].each do |sub_attr, value|
-        squish_and_strip(record[attribute], sub_attr)
+        squish_and_strip(object: record[attribute], attribute: sub_attr)
 
         # Deleting empty keys so that our jsonb column doesn't get cluttered up with empty values.
         record[attribute].delete(sub_attr) if value.blank?
       end
     else
-      squish_and_strip(record, attribute)
+      squish_and_strip(object: record, attribute: attribute)
     end
   end
 
-  def self.squish_and_strip(object, attribute)
+  def self.squish_and_strip(object:, attribute:)
     value = object[attribute]
 
     value.gsub!(/[[:space:]]+/, ' ') if value.respond_to?(:gsub!)
