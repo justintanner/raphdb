1fdcdaaf5bb3e5e437a7406f85d63ca957258c37
Justin Tanner <work@jwtanner.com>
2022-03-25 20:51:05 -0600

    Turn on infinite scrolling for medium search

---
+++ app/javascript/controllers/loadmore_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="loadmore"
+export default class extends Controller {
+  connect() {
+    this.createObserver();
+  }
+
+  createObserver() {
+    const observer = new IntersectionObserver(
+      entries => this.handleIntersect(entries),
+      {
+        // https://github.com/w3c/IntersectionObserver/issues/124#issuecomment-476026505
+        threshold: [0, 1.0],
+      }
+    )
+    observer.observe(this.element);
+  }
+  handleIntersect(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.element.click();
+      }
+    })
+  }
+}
