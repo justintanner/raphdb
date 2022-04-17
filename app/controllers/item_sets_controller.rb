d58b165be2be39cd28f25c643ce65bd21f5080c4
Justin Tanner <work@jwtanner.com>
2022-04-17 13:06:29 -0600

    Item sets now infinite scroll as well

--- app/controllers/item_sets_controller.rb
+++ app/controllers/item_sets_controller.rb
@@ -5,7 +5,7 @@ class ItemSetsController < ApplicationController
 
   def show
     @item_set = ItemSet.friendly.find(params[:id])
-    @pagy, @items = pagy(@item_set.items, items: per_page)
+    @pagy, @items = pagy(@item_set.items.with_attached_images_and_variants, items: per_page)
     @tab = tab
   end
 end
