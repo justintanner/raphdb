59aee383b57d45ab12cc88eef81e8e92efeaf081
Justin Tanner <work@jwtanner.com>
2022-06-01 18:10:13 -0600

    Finished editor search clear button

---
+++ app/javascript/controllers/editor_search_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="editor-search"
+export default class extends Controller {
+  static targets = ["input"]
+
+  connect() {
+    const that = this
+
+    that.inputTarget.onkeydown = (event) => {
+      if (that.isEscape(event)) {
+        that.clear();
+      }
+    };
+  }
+
+  isEscape(event) {
+    if ("key" in event) {
+      return (event.key === "Escape" || event.key === "Esc")
+    } else {
+      return (event.keyCode === 27)
+    }
+  }
+
+  clear() {
+    const that = this
+
+    that.inputTarget.value = ""
+    that.element.requestSubmit()
+  }
+}
