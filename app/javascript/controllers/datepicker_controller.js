6df9b809574d73f6eb924a599188939fa3c804ab
Justin Tanner <work@jwtanner.com>
2022-06-29 00:16:27 -0600

    Fixed bug in filters date picker and set better defaults

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,11 +3,22 @@ import "vanillajs-datepicker"
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
-  static values = {format: String};
+  static values = {format: String}
 
   connect() {
-    const datepicker = new Datepicker(this.element, {
+    const that = this
+
+    that.datepicker = new Datepicker(that.element, {
+      showDaysOfWeek: false,
+      autohide: true,
+      maxView: 3,
+      startView: 2,
       format: this.formatValue,
+    })
+
+    that.element.addEventListener("changeDate", function (_e, _details) {
+      // This will get picked up by the filter-controller.
+      that.element.dispatchEvent(new Event("input"))
     })
   }
 }
