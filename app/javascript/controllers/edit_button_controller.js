4c9f9e2d3308beaf822c8665764d6fd9a32d59ff
Justin Tanner <work@jwtanner.com>
2022-07-21 15:44:08 -0600

    Added extra save function for off canvas

--- app/javascript/controllers/edit_button_controller.js
+++ app/javascript/controllers/edit_button_controller.js
@@ -15,9 +15,18 @@ export default class extends Controller {
   }
 
   showOffCanvas() {
+    const frame = document.getElementById("edit_item_frame")
     const offCanvasElement = document.getElementById("edit_item_offcanvas")
     const bsOffcanvas = new bootstrap.Offcanvas(offCanvasElement)
     bsOffcanvas.show()
+
+    // Save data, If the user edits and quickly closes the offcanvas.
+    offCanvasElement.addEventListener("hidden.bs.offcanvas", () => {
+      const form = frame.querySelector("form")
+      if (form) {
+        form.requestSubmit();
+      }
+    })
   }
 }
 
