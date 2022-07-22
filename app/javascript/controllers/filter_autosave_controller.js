a73e15420360483ed8073bd74e8bbff6e0d21420
Justin Tanner <work@jwtanner.com>
2022-07-21 19:18:06 -0600

    Created new autosave controller for filters

--- app/javascript/controllers/filter_autosave_controller.js
+++ app/javascript/controllers/filter_autosave_controller.js
@@ -3,16 +3,20 @@ import {useDebounce} from "stimulus-use"
 import {FetchRequest} from "@rails/request.js"
 import toastError from "../toast_error"
 
-// Connects to data-controller="autosave"
+// Connects to data-controller="filter-autosave"
 export default class extends Controller {
   static targets = ["form"]
-  static debounces = ["submit", "xhr"]
+  static debounces = ["submit"]
 
   connect() {
     useDebounce(this, {wait: 250})
   }
 
   submit() {
+    this.formTarget.requestSubmit()
+  }
+
+  xhr() {
     this.submitFormWithXhr()
   }
 
@@ -28,39 +32,10 @@ export default class extends Controller {
     const request = new FetchRequest(method, form.action + ".json", {body: new FormData(form)})
     const response = await request.perform()
 
-    this.clearAllErrors()
-
     if (response.statusCode === 422) {
-      const json = await response.json
-      this.displayErrors(json)
+      form.requestSubmit()
     } else if (response.statusCode !== 200) {
       toastError(this, response)
     }
-  }
-
-  displayErrors(json) {
-    json.errors.forEach(error => {
-      const field = document.getElementById(error.id)
-      const feedback = document.getElementById(error.id + "_feedback")
-
-      if (field) {
-        field.classList.add("is-invalid")
-      }
-      if (feedback) {
-        feedback.innerHTML = error.message
-      }
-    })
-  }
-
-  clearAllErrors() {
-    const fields = document.querySelectorAll(".is-invalid")
-    fields.forEach(field => {
-      field.classList.remove("is-invalid")
-    })
-
-    const feedbacks = document.querySelectorAll(".invalid-feedback")
-    feedbacks.forEach(feedback => {
-      feedback.innerHTML = ""
-    })
   }
 }
